edb4514e8a8679112fff3b722985966d87550a1c
SHurtado98 <sebastian.hurtado@utec.edu.pe>
2019-09-14 02:42:23 -0500

    randomfile listo, lector csv por empezar

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include "RandomFile.h"
 
 int main(){
+    /*
+    Header con funciones para pasar de CSV a un array de objetos record.
+    Por que funcion y no clase? No vas a hacer ningún cambio de estado, así que no es necesario tenerlo en clase.
+    funcion (fileName, ptrAlArray)
+    getline ',' y llenar elemento por elemento.
+    for para insertar
+    liberar espacio en la memoria
+    seguir con el siguiente csv
+
+
+
+    */
     CrimeRecord recordA;
     recordA.setData();
     RandomFile <CrimeRecord> randomA("data");
